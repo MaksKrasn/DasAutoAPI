2aae46fa91b52b1e05c0eb5b8732a839ea94f571
MaksKrasn <uw5ekr@gmail.com>
2020-09-20 16:31:36 +0300

    Modified host and port in config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 module.exports = {
-    port: 3000,
-    host: '127.0.0.1',
+    //port: 3000,
+    port: '',
+    //host: '127.0.0.1',
+    host: '',
     //dbUri: 'mongodb://localhost:27017/CarsDB'
     dbUri: 'mongodb+srv://admin:ae765736@clusterstepuw5ekr.yrr3g.mongodb.net/CarsDB?retryWrites=true&w=majority'
 }
