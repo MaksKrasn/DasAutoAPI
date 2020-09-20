a8f90e49ae68bac48ff5847424daba603f5accee
MaksKrasn <uw5ekr@gmail.com>
2020-09-20 15:13:53 +0300

    Modified config - add connect to cloud DB

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: 3000,
     host: '127.0.0.1',
-    dbUri: 'mongodb://localhost:27017/CarsDB'
+    //dbUri: 'mongodb://localhost:27017/CarsDB'
+    dbUri: 'mongodb+srv://admin:ae765736@clusterstepuw5ekr.yrr3g.mongodb.net/CarsDB?retryWrites=true&w=majority'
 }
